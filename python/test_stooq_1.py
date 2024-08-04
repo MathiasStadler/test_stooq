10909809c2d8cf9f14bee377d112ade6e2781147
Mathias <email@example.com>
2024-08-04 13:47:27 +0000

    save

--- python/test_stooq_1.py
+++ python/test_stooq_1.py
@@ -8,8 +8,10 @@ df = dfr.read()
 print(df.head())
 
 
+# debian 12
+# sudo apt update
+# sudo apt install python3 python3-dev virtualenv
+# sudo apt install libxml2 libxslt-dev
+
 # virtualenv my_project_env
 # source my_project_env/bin/activate
-
-
-# sudo apt install python3 python3-dev
