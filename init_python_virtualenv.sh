c39b712a8c147b901c3a916ef3817eb413ddd455
Mathias <email@example.com>
2024-08-05 09:16:48 +0000

    add

---
+++ init_python_virtualenv.sh
@@ -0,0 +1,10 @@
+virtualenv my_project_env
+source my_project_env/bin/activate
+pip install --upgrade pip
+
+# check yor inside env
+pip install --upgrade setuptools wheel
+pip install pandas_datareader
+
+cd python
+python3 test_stooq_1.py
